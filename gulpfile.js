a5a3c0842a88e5d51e5883aa54c833f161d9adf7
ReverseGTLT <dzewrpifzc@gmail.com>
2023-01-19 15:37:24 +0200

    finish priject

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+
+function copyHtmlTask() {
+    return src('./src/index.html')
+        .pipe(dest('./dist'))
+}
+function copyCssTask() {
+    return src('./src/style.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest('./dist'))
+}
+function copyJsTask() {
+    return src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'src/StudentsApi.js',
+        'src/index.js',
+    ])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(dest('./dist'))
+}
+function serveTask(done) {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    watch('./src/index.html', series (copyHtmlTask, browserReloadTask));
+    watch('./src/index.css', series (copyCssTask, browserReloadTask));
+    watch('./src/*js', series (copyJsTask, browserReloadTask));
+    done();
+}
+function browserReloadTask(done) {
+    browserSync.reload();
+    done();
+}
+function buildTask() {
+    return parallel(copyHtmlTask, copyCssTask, copyJsTask)
+}
+
+module.exports = {
+    build: buildTask(),
+    start: series(buildTask(), serveTask),
+}
